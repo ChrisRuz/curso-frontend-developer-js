7af680608e40aa94638d5b661b748abfe2028fe3
Chris Ruz <le17081417@merida.tecnm.mx>
2023-03-10 17:12:26 -0600

    Se configuro todo el navbar del menu y el carrito

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 const nav_Email = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarrito = document.querySelector('.navbar-shopping-cart');
+const aside =document.querySelector('.product-detail');
 
 nav_Email.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click', toggleAside);
 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    mobileMenu.classList.toggle('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+}
+
+function toggleAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')  
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+}
+
+
